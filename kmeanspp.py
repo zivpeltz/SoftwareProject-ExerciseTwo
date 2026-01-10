95dcea887790143c9490402c1423bd26d127b283
zivpeltz <zivpeltz@gmail.com>
2026-01-10 13:14:47 +0200

    added parse points and input edge case handle

--- kmeanspp.py
+++ kmeanspp.py
@@ -8,9 +8,39 @@ def calculate_distance(p,q): #p and q are arrays representing points
         sum_of_deltas += (p[i] - q[i]) ** 2
     return sum_of_deltas**0.5
 
-def kmeansplus(k,points_arr):
+def kmeans_plus(k,points_arr):
+    pass
 
 
+def is_float(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+
+def parse_points(file_A, file_B):
+    points_A = pd.read_csv(file_A, header=None)
+    points_B = pd.read_csv(file_B, header=None)
+
+    merged_points = pd.merge(points_A, points_B, how='inner' , on=0)
+    merged_points = merged_points.sort_values(merged_points.columns[0])
+
+    num_of_points = merged_points.shape[0]
+    dim = merged_points.shape[1]
+    points_arr = []
+    for i in range(num_of_points):
+        temp_point = []
+        for j in range(1 , dim): #start from 1 since key is included
+            temp_data = merged_points.iloc[i,j]
+            if not is_float(temp_data):
+                print("An Error Has Occurred")
+                raise SystemExit(1)
+            temp_point.append(float(temp_data))
+        points_arr.append(temp_point)
+
+    return points_arr
 
 
 
@@ -18,6 +48,7 @@ def kmeansplus(k,points_arr):
 
 def main():
     #reading user CMD arguments
+    provided_iter = 1 #in the case when iter is provided, the fields change index by 1
     if len(sys.argv) == 1 or len(sys.argv) > 6:
         print("An Error Has Occurred")
         raise SystemExit(1)
@@ -29,7 +60,7 @@ def main():
 
     k = int(k)
 
-    if len(sys.argv)==5:
+    if len(sys.argv) == 6:
         iter = sys.argv[2]
         if not iter.isdigit():
             print("Incorrect maximum iteration!")
@@ -39,8 +70,36 @@ def main():
             print("Incorrect maximum iteration!")
             raise SystemExit(1)
     else:
-        iter = 400
-    eps = sys.argv[3]
+        iter = 300
+        provided_iter = 0
+
+
+    eps = sys.argv[2 + provided_iter]
+    if not is_float(eps):
+        print("Incorrect epsilon!")
+        raise SystemExit(1)
+
+    eps = float(eps)
+    if eps < 0:
+        print("Incorrect epsilon!")
+        raise SystemExit(1)
+
+    file_name_a = sys.argv[3 + provided_iter] #we assume these are valid
+    file_name_b = sys.argv[4 + provided_iter]
+
+    points_arr = parse_points(file_name_a, file_name_b)
+    print(points_arr)
+
+    N = len(points_arr)
+
+    if N <= 1:
+        print("An Error Has Occurred")
+        raise SystemExit(1)
+
+    if k < 2 or k > N - 1: #final check on k, depends on the length of the points array
+        print("Incorrect number of clusters!")
+        raise SystemExit(1)
+
 
 
 if __name__=="__main__":
