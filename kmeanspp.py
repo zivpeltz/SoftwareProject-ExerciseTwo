815496a4289eff6a6c6bac8f02c538755d27c45d
LBornstein <lior.bornstein@gmail.com>
2026-01-10 12:22:29 +0200

    kmeansplus init function added

--- kmeanspp.py
+++ kmeanspp.py
@@ -8,8 +8,31 @@ def calculate_distance(p,q): #p and q are arrays representing points
         sum_of_deltas += (p[i] - q[i]) ** 2
     return sum_of_deltas**0.5
 
-def kmeansplus(k,points_arr):
+def calculate_min_distance(centers, p):
+    min = calculate_distance(centers[0], p)
+    for i in range(1,len(centers)):
+        temp = calculate_distance(centers[i], p)
+        if temp < min:
+            min = temp
+    return min
 
+def kmeansplus_Init(k, points_arr):
+    x = np.random.choice(points_arr)
+    centers=[x]
+    for i in range (k-1):
+        distance_arr = [calculate_min_distance(centers, p) for p in points_arr]
+        sum = sum(distance_arr)
+        prob_arr = [distance_arr[n]/sum for n in range(len(points_arr))]
+        centers.append(np.random.choice(points_arr, p=prob_arr))
+    return centers
+    
+    
+        
+
+
+        
+
+        
 
 
 
@@ -17,6 +40,7 @@ def kmeansplus(k,points_arr):
 
 
 def main():
+    np.random.seed(1234)
     #reading user CMD arguments
     if len(sys.argv) == 1 or len(sys.argv) > 6:
         print("An Error Has Occurred")
